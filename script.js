3033f81c9acb50edde59f970dbacff88f6ad20cf
Jorge Sosa <jorgeduardo.1994@gmail.com>
2017-10-18 17:25:52 -0500

    improved fussion function, added checker function

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 var balls = [];
 
 function setup (){
-	createCanvas(500, 500);
-	for (var i = 0; i < 9; i++) {
-		balls[i] = new Ball;
+	createCanvas(1200, 700);
+	for (var i = 0; i < 400; i++) {
+		balls[i] = new Ball(random(50,1150), random(50, 650), random (-5,5), random(-5,5), 8);
 	}
 	console.log(balls);
 }
@@ -11,16 +11,16 @@ function setup (){
 function draw(){
 	background(0);
 	// atomicBomb(balls);
+	resetFussion();
 	for (var i = 0; i < balls.length; i++) {
 		balls[i].display(i);
 		balls[i].move();
 		balls[i].bounce();
 		// balls[i].reproduce(balls);
-		balls[i].fussion(balls);
+		balls[i].fussion(balls, i);
 		// balls[i].lifespan(i);
 	}
-	// console.log(balls.length);
-
+	checker();
 }
 
 function mousePressed(){
@@ -28,19 +28,19 @@ function mousePressed(){
 	noLoop();
 }
 
-function Ball(){
+function Ball(posX, posY, velX, velY, size){
 	this.id = 0;
-	this.size = random(1,45);
+	this.size = size;
 	//colors 
 	this.r = random(0,255);
 	this.g = random(0,255);
 	this.b = random(0,255);
 	// this.alpha = 255;
 
-	this.x = random(50,450);
-	this.y = random(50,450);
-	this.velX = random (-5,5);
-	this.velY = random(-5,5);
+	this.x = posX;
+	this.y = posY;
+	this.velX = velX;
+	this.velY = velY;
 	this.onTop = true;
 	this.reproduced = false;
 	this.age = 1;
@@ -87,17 +87,15 @@ function Ball(){
 	}
 	this.fussion = function(objArray, current){
 		for(i=0;i<objArray.length;i++){
-			if(this.id != i){ //dont do the check if it is looking at itself
+			if(this.id != objArray[i].id){ //dont do the check if it is looking at itself
 				this.onTop = collideCircleCircle(this.x, this.y, this.size, objArray[i].x, objArray[i].y, objArray[i].size); //colliding with anything?
 				if(this.onTop == true){ // if we ever get a true we have to try again, this works since we iterate down through the objects one by one.
 					if(this.size > objArray[i].size){
-						this.size += objArray[i].size;
+						this.size += objArray[i].size/5;
 						balls.splice(i, 1);
-						console.log(balls);
 					}else{
-						objArray[i].size += this.size;
+						objArray[i].size += this.size/2;
 						balls.splice(current, 1);
-						console.log(balls);
 					}
 				}	
 			}
@@ -115,6 +113,27 @@ function Ball(){
 function atomicBomb(objArray){
 	if(objArray.length > 100){
 		objArray.splice(0, objArray.length);
+	}
+}
+
+function resetFussion(){
+	if(balls.length == 1){
+		 var lastPosx = balls[0].x;
+		 var lastPosY = balls[0].y;
+		 var lasSize = balls[0].size/2;
+		balls.splice(0, 1);
+		for (var i = 0; i < 500; i++) {
+			balls[i] = new Ball(lastPosx+Math.ceil(random(lasSize*-1,lasSize)), lastPosY+Math.ceil(random(lasSize*-1,lasSize)), random (-5,5), random(-5,5), 5);
+		}
+		console.log(balls);
+	}
+}
+
+function checker(){
+	for (var i = 0; i < balls.length; i++) {
+		if(balls[i].x < -100 || balls[i].x > width+100 || balls[i].y < -100 || balls[i].y > height+100){
+			balls.splice(i, 1);
+		}
 	}
 }
 
